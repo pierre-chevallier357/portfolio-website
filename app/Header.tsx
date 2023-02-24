381d90df48eb877bd3e7434e94a9b33a9dbee9c3
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-02-24 22:29:04 +0100

    Change social icons' color to white

--- app/Header.tsx
+++ app/Header.tsx
@@ -6,11 +6,11 @@ export default function Header() {
   return (
     <header className="navbar">
       <div className="social-icons">
-        <SocialIcon url="https://www.linkedin.com/in/pierre-chevallier/" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="https://github.com/pierre-chevallier357" fgColor="gray" bgColor="transparent" />
+        <SocialIcon url="https://www.linkedin.com/in/pierre-chevallier/" fgColor="white" bgColor="transparent" />
+        <SocialIcon url="https://github.com/pierre-chevallier357" fgColor="white" bgColor="transparent" />
       </div>
       <div className="contact-me">
-        <SocialIcon url="mailto: contact@pierre-chevallier.com" fgColor="gray" bgColor="transparent" />
+        <SocialIcon url="mailto: contact@pierre-chevallier.com" fgColor="white" bgColor="transparent" />
         <p className="get-in-touch">GET IN TOUCH</p>
       </div>
     </header>
