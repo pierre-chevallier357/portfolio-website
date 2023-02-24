11df14dfb2d29ab0818d852594fc649f2ec6b0a7
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-02-24 22:30:59 +0100

    Add particles animation to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Header from './Header';
+import Script from 'next/script';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <h1>Hello world!</h1>
+        <section>
+          <div id="particles-js" className={styles.particles} />
+          <h1>Hello world!</h1>
+          <Script src="particles.js" />
+        </section>
       </main>
     </>
   );
