2a8f33ea3040223e54636b5f64534710d23313ac
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-02-27 21:52:08 +0100

    Add a short description to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.scss';
 
 export const metadata = {
   title: "Pierre Chevallier's Portfolio",
-  description: '',
+  description: "Pierre Chevallier's Portfolio Website",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
