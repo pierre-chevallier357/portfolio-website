74a6bf461dfeab00fe8d4c56637a7627c5bd60e5
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-02-27 21:05:07 +0100

    Remove social icons and add HTML structure

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,17 +1,25 @@
-import { SocialIcon } from 'react-social-icons';
 import './Header.scss';
 
 export default function Header() {
   return (
     <header className="navbar">
-      <div className="social-icons">
-        <SocialIcon url="https://www.linkedin.com/in/pierre-chevallier/" fgColor="white" bgColor="transparent" />
-        <SocialIcon url="https://github.com/pierre-chevallier357" fgColor="white" bgColor="transparent" />
-      </div>
-      <div className="contact-me">
-        <SocialIcon url="mailto: contact@pierre-chevallier.com" fgColor="white" bgColor="transparent" />
-        <p className="get-in-touch">GET IN TOUCH</p>
-      </div>
+      <nav>
+        <div>
+          <a href=""></a>
+        </div>
+        <div>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </div>
+        <div>
+          <a href=""></a>
+        </div>
+      </nav>
     </header>
   );
 }
