d6f0c483c5592b3c75b29eacff3e6e1022750993
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-02-27 21:05:35 +0100

    Update to Next 13.2.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import './globals.scss';
 
+export const metadata = {
+  title: "Pierre Chevallier's Portfolio",
+  description: '',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
       <body>{children}</body>
     </html>
   );
