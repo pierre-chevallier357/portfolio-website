109c097625d46196e98f2d50612c0acdbc8c1c98
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-03-03 19:30:47 +0100

    Add logo and hamburger button on header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,24 +1,25 @@
 import './Header.scss';
+import HamburgerButton from './HamburgerButton/HamburgerButton';
+import Image from 'next/image';
+import pierreDrawing from '/public/header/pierre-drawing.png';
 
 export default function Header() {
   return (
-    <header className="navbar">
-      <nav>
-        <div>
-          <a href=""></a>
+    <header>
+      <nav className="navbar">
+        <a href="">
+          <Image src={pierreDrawing} alt="Dessin simplifié de Pierre" className="pierre-drawing" />
+        </a>
+        <div className="links-wrapper">
+          <a href="">Accueil</a>
+          <a href="">À propos</a>
+          <a href="">Compétences</a>
+          <a href="">Expériences</a>
+          <a href="">Projets</a>
+          <a href="">Études</a>
+          <a href="">Contact</a>
         </div>
-        <div>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-        </div>
-        <div>
-          <a href=""></a>
-        </div>
+        <HamburgerButton />
       </nav>
     </header>
   );
