f3916b0a26454ede5fcc59335331ea4b9bbb1cd1
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-03-03 22:37:03 +0100

    Refactor Header and add hamburger button

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,25 +1,55 @@
+'use client';
 import './Header.scss';
-import HamburgerButton from './HamburgerButton/HamburgerButton';
 import Image from 'next/image';
 import pierreDrawing from '/public/header/pierre-drawing.png';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Header() {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  function toggleMenu(): void {
+    setIsMenuOpened((isMenuOpened) => !isMenuOpened);
+  }
+
+  function closeMenu(): void {
+    setIsMenuOpened(false);
+  }
+
   return (
     <header>
       <nav className="navbar">
-        <a href="">
+        <Link href="/">
           <Image src={pierreDrawing} alt="Dessin simplifié de Pierre" className="pierre-drawing" />
-        </a>
+        </Link>
         <div className="links-wrapper">
-          <a href="">Accueil</a>
-          <a href="">À propos</a>
-          <a href="">Compétences</a>
-          <a href="">Expériences</a>
-          <a href="">Projets</a>
-          <a href="">Études</a>
-          <a href="">Contact</a>
+          <Link href="/" onClick={closeMenu}>
+            Accueil
+          </Link>
+          <Link href="" onClick={closeMenu}>
+            À propos
+          </Link>
+          <Link href="" onClick={closeMenu}>
+            Compétences
+          </Link>
+          <Link href="" onClick={closeMenu}>
+            Expériences
+          </Link>
+          <Link href="" onClick={closeMenu}>
+            Projets
+          </Link>
+          <Link href="" onClick={closeMenu}>
+            Études
+          </Link>
+          <Link href="" onClick={closeMenu}>
+            Contact
+          </Link>
         </div>
-        <HamburgerButton />
+        <button className={isMenuOpened ? 'hamburger-button opened' : 'hamburger-button'} onClick={toggleMenu}>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </button>
       </nav>
     </header>
   );
