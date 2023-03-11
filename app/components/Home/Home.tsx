0447e3e80828f5c062c5c1f955f7749df7c8599c
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-03-11 22:09:20 +0100

    Add home page title (with dynamic text)

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,10 +1,25 @@
+'use client';
 import Script from 'next/script';
 import './Home.scss';
+import { Typewriter } from 'react-simple-typewriter';
 
 export default function Home() {
   return (
     <>
-      <h1 className="title">Hello world!</h1>
+      <h1 className="title">
+        Hey !
+        <br />
+        Je suis Pierre Chevallier,
+        <br />
+        Je suis{' '}
+        <Typewriter
+          words={['développeur web.', 'étudiant en M2 MIAGE.']}
+          cursor
+          cursorColor="#56dcfc"
+          loop={true}
+          delaySpeed={2000}
+        />
+      </h1>
       <div id="particles-js" className="particles" />
       <Script src="home/particles/particles.min.js" />
     </>
