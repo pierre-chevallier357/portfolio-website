4a5a061fc24a29c9c2c0e30d8004c296df04512a
Pierre Chevallier <pierre.chevallier2000@orange.fr>
2023-03-14 22:24:08 +0100

    Add About button on home page

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -4,21 +4,41 @@ import './Home.scss';
 import { Typewriter } from 'react-simple-typewriter';
 import profilePicture from '/public/images/home/profile-picture.png';
 import Image from 'next/image';
+import chevronDown from '/public/images/home/chevron-down-solid.svg';
+import arrowDown from '/public/images/home/arrow-down-solid.svg';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isAboutHovered, setIsAboutHovered] = useState(false);
+
   return (
     <>
-      <div className="presentation">
-        <h1 className="title">
-          Hey !
-          <br />
-          Je m'appelle <span className="name">Pierre Chevallier</span>,
-          <br />
-          Je suis{' '}
-          <span className="job">
-            <Typewriter words={['développeur web', 'étudiant en M2 MIAGE']} cursor loop={true} delaySpeed={2000} />
-          </span>
-        </h1>
+      <div className="hero">
+        <div className="presentation">
+          <h1 className="title">
+            Hey !
+            <br />
+            Je m'appelle <span className="name">Pierre Chevallier</span>,
+            <br />
+            Je suis{' '}
+            <span className="job">
+              <Typewriter words={['développeur web', 'étudiant en M2 MIAGE']} cursor loop={true} delaySpeed={2000} />
+            </span>
+          </h1>
+          <button
+            className="about-button"
+            onMouseEnter={() => setIsAboutHovered(true)}
+            onMouseLeave={() => setIsAboutHovered(false)}>
+            À propos
+            <Image
+              src={isAboutHovered ? arrowDown : chevronDown}
+              alt="Flèche pointant vers le bas"
+              width={16}
+              className="arrow-down"
+              priority={true}
+            />
+          </button>
+        </div>
         <Image
           src={profilePicture}
           alt="Photo de Pierre"
